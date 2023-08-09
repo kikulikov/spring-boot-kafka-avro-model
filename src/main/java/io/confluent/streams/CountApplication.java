0f4fafa8354128f398b4d7f304d1000e00f62c3a
Kirill Kulikov <kikulikov@@users.noreply.github.com>
2023-08-09 23:46:39 +0100

    updated

--- src/main/java/io/confluent/streams/CountApplication.java
+++ src/main/java/io/confluent/streams/CountApplication.java
@@ -1,12 +1,7 @@
 package io.confluent.streams;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class CountApplication {
