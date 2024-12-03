b323e90a04d7b14302929a29e5c230625b1c2a80
Kirill Kulikov <2751459+kikulikov@users.noreply.github.com>
2024-12-03 14:36:31 +0000

    versions bump up

--- src/main/java/io/confluent/datasource/DataSourceFaker.java
+++ src/main/java/io/confluent/datasource/DataSourceFaker.java
@@ -1,8 +1,8 @@
 package io.confluent.datasource;
 
-import com.github.javafaker.Faker;
 import io.confluent.model.avro.Book;
 import io.confluent.model.avro.Order;
+import net.datafaker.Faker;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -10,17 +10,17 @@ import java.util.UUID;
 @Component
 public class DataSourceFaker implements DataSource {
 
-    private static final Faker FAKER = Faker.instance();
+  private static final Faker FAKER = new Faker();
 
-    @Override
-    public Book nextBook() {
-        final var title = FAKER.harryPotter().book();
-        return new Book(UUID.nameUUIDFromBytes(title.getBytes()).toString(), title);
-    }
+  @Override
+  public Book nextBook() {
+    final var title = FAKER.harryPotter().book();
+    return new Book(UUID.nameUUIDFromBytes(title.getBytes()).toString(), title);
+  }
 
-    @Override
-    public Order nextOrder() {
-        return new Order(FAKER.internet().uuid(), nextBook().getBookId(),
-                FAKER.random().nextInt(1, 10), FAKER.finance().creditCard());
-    }
+  @Override
+  public Order nextOrder() {
+    return new Order(FAKER.internet().uuid(), nextBook().getBookId(),
+        FAKER.random().nextInt(1, 10), FAKER.finance().creditCard());
+  }
 }
